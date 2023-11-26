47135f419971e9872f8d5db2118e2995f5c18db0
joeCodes05 <nathancodes05@gmail.com>
2023-11-26 18:54:26 +0100

    project init - creact-react-app & installed tailwindcss

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/home'
+
+const App = () => {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+        </Routes>
+      </Router>
+    </>
+  )
+}
+
+export default App
